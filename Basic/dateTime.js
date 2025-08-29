49431d9a7a9a83e1065b15cdfdf92f6443b383c8
Prathmesh N S <74286521+PrathmeshNS@users.noreply.github.com>
2025-08-29 08:22:25 +0000

    date time

---
+++ Basic/dateTime.js
@@ -0,0 +1,11 @@
+let myDate = new Date()
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleString());
+
+// let myCreaateDate = new Date(2003,11,8)
+
+
+let myCreaateDate = new Date(2003,11,8,2,15)
+console.log(myCreaateDate.toLocaleString());
