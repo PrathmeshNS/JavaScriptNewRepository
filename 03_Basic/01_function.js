bc85e44c7db68810f77bdce4b39802941950f008
Prathmesh N S <74286521+PrathmeshNS@users.noreply.github.com>
2025-08-30 14:32:08 +0000

    some changes into the filename

---
+++ 03_Basic/01_function.js
@@ -0,0 +1,15 @@
+//functions
+
+
+function sayMyName() {
+    console.log("P");
+    console.log("R");
+    console.log("A");
+    console.log("T");
+    console.log("H");
+    console.log("M");
+    console.log("E");
+    console.log("S");
+    console.log("H");
+}
+
